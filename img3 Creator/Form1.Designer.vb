3fb5b5ebc3dc7074f5f78ed74bd79e138d6d1fcd
Stanley Hutcheon <stanley.h@hotmail.co.uk>
2011-03-28 17:29:38 +0100

    v1b1

---
+++ img3 Creator/Form1.Designer.vb
@@ -0,0 +1,289 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.img3 = New System.Windows.Forms.OpenFileDialog()
+        Me.png = New System.Windows.Forms.OpenFileDialog()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.tick2 = New System.Windows.Forms.PictureBox()
+        Me.tick1 = New System.Windows.Forms.PictureBox()
+        Me.saveimg3 = New System.Windows.Forms.SaveFileDialog()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.tick2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.tick1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Controls.Add(Me.tick2)
+        Me.Panel1.Controls.Add(Me.tick1)
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.TextBox5)
+        Me.Panel1.Controls.Add(Me.LinkLabel1)
+        Me.Panel1.Controls.Add(Me.TextBox4)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.TextBox3)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.TextBox2)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Controls.Add(Me.label1)
+        Me.Panel1.Location = New System.Drawing.Point(2, 4)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(281, 340)
+        Me.Panel1.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(102, 274)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Browse"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox5
+        '
+        Me.TextBox5.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox5.Location = New System.Drawing.Point(3, 243)
+        Me.TextBox5.Multiline = True
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.ReadOnly = True
+        Me.TextBox5.Size = New System.Drawing.Size(273, 34)
+        Me.TextBox5.TabIndex = 9
+        Me.TextBox5.Text = "Then browse for the .png (image) you wish to inject into the img3"
+        Me.TextBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(66, 158)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(146, 13)
+        Me.LinkLabel1.TabIndex = 8
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "iPhoneWiki - VFDecrypt Keys"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox4.Location = New System.Drawing.Point(3, 113)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.ReadOnly = True
+        Me.TextBox4.Size = New System.Drawing.Size(273, 47)
+        Me.TextBox4.TabIndex = 7
+        Me.TextBox4.Text = "Please enter the key/iv for the applelogo you just selected" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "You can get these on" & _
+            " the iPhoneWiki"
+        Me.TextBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(102, 84)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Browse"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox3.Location = New System.Drawing.Point(3, 35)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ReadOnly = True
+        Me.TextBox3.Size = New System.Drawing.Size(273, 43)
+        Me.TextBox3.TabIndex = 5
+        Me.TextBox3.Text = "Please select a stock AppleLogo.img3;" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "You can get this if you extract firmware a" & _
+            "nd go to location:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "/Firmware/all_flash/all_flash.(device).production"
+        Me.TextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(10, 200)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(15, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "iv"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(10, 158)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(24, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "key"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox2.Location = New System.Drawing.Point(3, 217)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(273, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Location = New System.Drawing.Point(3, 177)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(273, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Font = New System.Drawing.Font("Lucida Sans", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label1.Location = New System.Drawing.Point(43, -1)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(192, 32)
+        Me.label1.TabIndex = 0
+        Me.label1.Text = "img3 Creator"
+        '
+        'img3
+        '
+        Me.img3.FileName = "applelogo"
+        Me.img3.Filter = "AppleLogo .img3|*.img3"
+        Me.img3.Title = "Please browse for a stock AppleLogo.img3"
+        '
+        'png
+        '
+        Me.png.Filter = "Image Files .png|*.png"
+        '
+        'Panel2
+        '
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel2.Controls.Add(Me.Button3)
+        Me.Panel2.Location = New System.Drawing.Point(-1, 303)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(281, 36)
+        Me.Panel2.TabIndex = 13
+        '
+        'Button3
+        '
+        Me.Button3.Enabled = False
+        Me.Button3.Location = New System.Drawing.Point(102, 3)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 0
+        Me.Button3.Text = "Inject"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(289, 4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(230, 340)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'tick2
+        '
+        Me.tick2.Image = Global.img3_Creator.My.Resources.Resources.green_tick_small
+        Me.tick2.Location = New System.Drawing.Point(183, 265)
+        Me.tick2.Name = "tick2"
+        Me.tick2.Size = New System.Drawing.Size(30, 32)
+        Me.tick2.TabIndex = 12
+        Me.tick2.TabStop = False
+        Me.tick2.Visible = False
+        '
+        'tick1
+        '
+        Me.tick1.Image = Global.img3_Creator.My.Resources.Resources.green_tick_small
+        Me.tick1.Location = New System.Drawing.Point(183, 75)
+        Me.tick1.Name = "tick1"
+        Me.tick1.Size = New System.Drawing.Size(30, 32)
+        Me.tick1.TabIndex = 11
+        Me.tick1.TabStop = False
+        Me.tick1.Visible = False
+        '
+        'saveimg3
+        '
+        Me.saveimg3.Filter = "img3|*.img3"
+        Me.saveimg3.Title = "Select a save location for your custom img3"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(522, 346)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "img3 Creator | v1b1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.tick2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.tick1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents label1 As System.Windows.Forms.Label
+    Friend WithEvents img3 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents png As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents tick1 As System.Windows.Forms.PictureBox
+    Friend WithEvents tick2 As System.Windows.Forms.PictureBox
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents saveimg3 As System.Windows.Forms.SaveFileDialog
+
+End Class
---
+++ img3 Creator/Form1.vb
@@ -0,0 +1,131 @@
+﻿Public Class Form1
+    Private Property winstyle As ProcessWindowStyle
+    Public temp = My.Computer.FileSystem.SpecialDirectories.Temp
+    Public key As String = ""
+    Public iv As String = ""
+
+    Public Sub imagetoolInject(ByVal png As String, ByVal destinationimg3 As String, ByVal templateimg3 As String, ByVal iv As String, ByVal key As String)
+        ShellWait(temp & "\imgtool\imagetool.exe", "inject " & png & " " & destinationimg3 & " " & templateimg3 & " " & iv & " " & key)
+    End Sub
+
+    Public Function killProc(ByVal processToKill As String)
+        For Each p As Process In Process.GetProcessesByName(processToKill)
+            p.Kill()
+        Next
+        Return 0
+    End Function
+
+    Public Sub ShellWait(ByVal file As String, ByVal arg As String)
+        Dim procNlite As New Process
+        winstyle = 1
+        procNlite.StartInfo.FileName = file
+        procNlite.StartInfo.Arguments = " " & arg
+        procNlite.StartInfo.WindowStyle = winstyle
+        Application.DoEvents()
+        procNlite.Start()
+        Do Until procNlite.HasExited
+            Application.DoEvents()
+            For i = 0 To 5000000
+                Application.DoEvents()
+            Next
+        Loop
+        procNlite.WaitForExit()
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ChDir(temp)
+        killProc("7za")
+        System.IO.File.WriteAllBytes(temp & "\imgtool.zip", My.Resources.imgtool)
+        System.IO.File.WriteAllBytes(temp & "\7za.exe", My.Resources._7za)
+        System.IO.File.WriteAllBytes(temp & "\extract.bat", My.Resources.extractimgtool)
+        'ShellWait(temp & "\7za.exe", "e " & temp & "\imgtool.zip")
+        If System.IO.Directory.Exists(temp & "\imgtool") Then
+            Try
+                My.Computer.FileSystem.DeleteDirectory(temp & "\imgtool", FileIO.DeleteDirectoryOption.DeleteAllContents)
+            Catch ex As Exception
+                Exit Sub
+            End Try
+        End If
+        ExecCmd(temp & "\extract.bat")
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Button1.Enabled = False
+        img3.ShowDialog()
+        If img3.FileName = "" Then
+            Exit Sub
+        End If
+        If System.IO.File.Exists(temp & "\template.img3") Then
+            System.IO.File.Delete(temp & "\template.img3")
+        Else
+            System.IO.File.Copy(img3.FileName, temp & "\template.img3")
+        End If
+
+        If System.IO.File.Exists(temp & "\bootlogo.img3") Then
+            System.IO.File.Delete(temp & "\bootlogo.img3")
+        Else
+            System.IO.File.Copy(img3.FileName, temp & "\bootlogo.img3")
+        End If
+        tick1.Visible = True
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Button2.Enabled = False
+        png.ShowDialog()
+        If png.FileName = "" Then
+            Exit Sub
+        End If
+        If System.IO.File.Exists(temp & "\picture.png") Then
+            System.IO.File.Delete(temp & "\picture.png")
+        Else
+            System.IO.File.Copy(png.FileName, temp & "\picture.png")
+        End If
+        PictureBox1.Image.FromFile(temp & "\picture.png")
+        tick1.Visible = True
+        Button3.Enabled = True
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Button3.Enabled = False
+        'ShellWait(temp & "\imgtool\imagetool.exe", "inject " & temp & "\picture.png " & temp & "\bootlogo.img3 " & temp & "\template.img3 " & key & " " & iv)
+        iv = TextBox2.Text
+        key = TextBox1.Text
+        imagetoolInject(temp & "\picture.png", temp & "\bootlogo.img3", temp & "\template.img3", iv, key)
+        saveimg3.ShowDialog()
+        If saveimg3.FileName = "" Then
+            MsgBox("your img3 has been created but because you didn't select a save area, it is located at:" & vbCrLf & _
+                   "%temp%\bootlogo.img3", MsgBoxStyle.Information, "img3 Creator | Help")
+            Exit Sub
+        End If
+        System.IO.File.WriteAllBytes(saveimg3.FileName, temp & "\bootlogo.img3")
+        MsgBox("Your newly created img3 is located at:" & vbCrLf & saveimg3.FileName)
+    End Sub
+
+    Private Sub TextBox2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox2.TextChanged
+        If TextBox2.Text = "" Then
+            Button2.Enabled = False
+        ElseIf Not TextBox1.Text = "" And Not TextBox2.Text = "" Then
+            Button2.Enabled = True
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+        If TextBox1.Text = "" Then
+            Button2.Enabled = False
+        ElseIf Not TextBox1.Text = "" And Not TextBox2.Text = "" Then
+            Button2.Enabled = True
+        End If
+    End Sub
+
+    Private Sub TextBox3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox3.Click
+        label1.Select()
+    End Sub
+
+    Private Sub TextBox4_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox4.TextChanged
+        [Select]()
+    End Sub
+
+    Private Sub TextBox5_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox5.TextChanged
+        label1.Select()
+    End Sub
+End Class
